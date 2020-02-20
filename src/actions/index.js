712a3b48107b8c2d86336caa9c298a934995f505
thenatural86 <philip.smalls86@gmail.com>
2020-02-20 06:36:45 -0500

    rendered song list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 //Action creator
-export const selectSong = () => {
+export const selectSong = song => {
   // return an action
   return {
     type: "SONG_SELECTED",
-    payload: "song"
+    payload: song
   }
 }
